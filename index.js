c5a49c5c5395d51194bf9eefd2b21f1875b0e1df
DuhaAbdullah <do7a.ab@gmail.com>
2022-08-08 00:19:38 +0500

    8/8/2022 12:19

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const data = require("./data.json");
 var cors = require("cors");
 const { Category } = require("./models/categories");
 const { User } = require("./models/user");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -80,24 +83,8 @@ app.get("/categories/:categoryId", async (req, res) => {
     });
 });
 
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  User.create(req.body)
-    .then((results) => {
-      res.send(results);
-      console.log(results);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-});
-
-app.post("/user/login", (req, res) => {
- 
-});
-
-
-app.get("/users", async (req, res) => {//this endpoint for admin dashboard
+//this endpoint for admin dashboard
+app.get("/users", async (req, res) => {
   const allUsers = await User.find({});
   res.send(allUsers);
   console.log("get users details");
@@ -122,6 +109,51 @@ app.get("/users/:userEmail", (req, res) => {
     })
     .catch((error) => {
       res.send(error);
+    });
+});
+
+app.post("/user/register", (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    User.create({ email, password: hash, firstName, lastName })
+      .then((results) => {
+        res.send(results);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  });
+});
+
+app.post("/user/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((results) => {
+      if (!results) {
+        res.status(404).send({
+          error: 404,
+          errorDescription: "Mongoose error",
+          message: "user not found, please try another entries",
+        });
+      } else {
+        bcrypt.compare(
+          password,
+          results.password,
+          function (err, compareResult) {
+            if (compareResult === true) {
+              res.send({ success: true, results });
+            } else {
+              res.status(401).send({
+                success: false,
+                message: "password or email is incorrect",
+              });
+            }
+          }
+        );
+      }
+    })
+    .catch((error) => {
+      res.send({ errorDescription: "Database Error", error: error });
     });
 });
 
